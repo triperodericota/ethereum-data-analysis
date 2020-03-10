f5b7786c6a1631703353258b2b57e65607f237f6
Nicolas Mc Cormack <nicolasmccormack22@gmail.com>
2020-03-10 17:19:25 -0300

    update json path

--- webapp/ethereum_analysis/ethereum_analysis_app/views.py
+++ webapp/ethereum_analysis/ethereum_analysis_app/views.py
@@ -18,7 +18,7 @@ import pdb
 import pandas as pd
 
 def retrieve_eth_usd_in(range_of_timestamps):
-    source = pd.read_json("../../../ETL/data_source/exchange_rates/eth_to_usd.json")
+    source = pd.read_json("../../ETL/data_source/exchange_rates/eth_to_usd.json")
     exchanges = source['Data']['Data']
     data_frame_data = {'date':[], 'value':[]}
     for elem in exchanges:
