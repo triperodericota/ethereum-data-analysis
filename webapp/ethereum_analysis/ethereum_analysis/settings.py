e7a63ef7306c9ced70026585b92c3f80cd0abde6
Nicolas Mc Cormack <nicolasmccormack22@gmail.com>
2020-03-10 13:21:18 -0300

    secret key for production

--- webapp/ethereum_analysis/ethereum_analysis/settings.py
+++ webapp/ethereum_analysis/ethereum_analysis/settings.py
@@ -16,12 +16,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
