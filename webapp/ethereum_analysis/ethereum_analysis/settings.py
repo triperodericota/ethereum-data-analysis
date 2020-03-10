8234c0cf329fdd04ae2fca7cbabbf88f615a0631
Nicolas Mc Cormack <nicolasmccormack22@gmail.com>
2020-03-10 14:27:58 -0300

    fix import error

--- webapp/ethereum_analysis/ethereum_analysis/settings.py
+++ webapp/ethereum_analysis/ethereum_analysis/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import dj_database_url
-import django-heroku
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
